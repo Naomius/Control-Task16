7cb41e78a312752c94b53405a0de2f91685c85dd
Igor G <106555763+Naomius@users.noreply.github.com>
2023-02-14 23:54:19 +0300

    someChanges

--- scripts/answers.js
+++ scripts/answers.js
@@ -4,12 +4,13 @@
         currentQuestIndex: 1,
         questTitleElem: null,
         optionElem: null,
+        rightQuizzes: [],
+        quizzes: [],
         init() {
             const url = new URL(location.href);
             const testId = url.searchParams.get('id');
-            const stringId = url.searchParams.get('answerId');
-            const answersIdMassive = stringId.split(' ');
-            
+            const stringId = url.searchParams.get('answerId').split(',', 6);
+            console.log(stringId)
             if (testId) {
                 const xhr = new XMLHttpRequest();
                 xhr.open('GET', 'https://testologia.site/get-quiz?id=' + testId, false);
@@ -28,46 +29,67 @@
             } else {
                 location.href = 'index.html';
             }
+
+            const xhr = new XMLHttpRequest();
+            const rightAnswerId = url.searchParams.get('id');
+            xhr.open('GET', 'https://testologia.site/get-quiz-right?id=' + testId, false);
+            xhr.send();
+
+            if (xhr.status === 200 && xhr.responseText) {
+                try {
+                    this.quizzes = JSON.parse(xhr.responseText);
+                } catch (e) {
+                    location.href = 'index.html';
+                }
+            } else {
+                location.href = 'index.html';
+            }
+            console.log(this.quizzes)
         },
         startQuizTest() {
+            document.getElementById('answer-title').innerText = this.quiz.name;
             this.questTitleElem = document.querySelector('.answer-question-title');
             this.optionElem = document.querySelector('.answer-question-options');
-            console.log(this.optionElem)
 
             console.log(this.quiz)
             this.showQuestions();
+
+          function checkAnswers() {
+           }
         },
         showQuestions() {
-            const activeQuestion = this.quiz.questions[this.currentQuestIndex - 1];
-            // activeQuestion.forEach((item, index) => {
-            //     item.questTitleElem.innerHTML =
-            // })
-            this.questTitleElem.innerHTML = '<span>Вопрос ' + this.currentQuestIndex
-                + ':</span> ' + activeQuestion.question;
+                // if (d) {
+                //
+                // }
+                let count = document.querySelector('.answer-question');
+               console.log(count.id)
 
-            activeQuestion.answers.forEach(answer => {
-                const optionElement = document.createElement('div');
-                optionElement.className = 'answer-question-option';
+            this.quiz.questions.forEach((q, i)=> {
+                const qWrapper = document.querySelector('.answer-wrapper');
+                const questionAnswer = `<div class="answer-question" id="${q.id}">
+                                           <div class="answer-question-title" id="title">
+                                               <span>Вопрос ${i + 1}:</span> ${q.question}
+                                           </div>
+                                           <div class="answer-question-options" id="options2">
+                                                ${renderAnswers(q.answers)}
+                                           </div>
+                                       </div>`
+                    qWrapper.innerHTML += questionAnswer;
+            });
 
-                const inputId = 'answer-' + answer.id;
-                const inputElement = document.createElement('input');
-                inputElement.setAttribute('id', inputId);
-                inputElement.setAttribute('type', 'radio');
-                inputElement.setAttribute('name', 'answer');
-                inputElement.setAttribute('value', answer.id);
-                inputElement.setAttribute('disabled', 'disabled');
+            function renderAnswers (answers) {
+              const result = answers.map(a => {
+                    const answer = `<div class="answer-question-option">
+                                        <input type="radio" id="${a.id}" disabled ='disabled'>
+                                        <label>${a.answer}</label>
+                                    </div>`;
+                    return answer;
+                });
 
-                const labelElement = document.createElement('label');
-                labelElement.setAttribute('for', inputId)
-                labelElement.innerText = answer.answer;
+              return result.join('');
+            }
+        },
 
-                optionElement.appendChild(inputElement);
-                optionElement.appendChild(labelElement);
-
-                this.optionElem.appendChild(optionElement);
-            })
-            // this.optionElem.innerHTML = '';
-        }
     }
     Answers.init();
 })();
